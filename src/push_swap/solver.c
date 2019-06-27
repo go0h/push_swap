152eb20d73d44d0a24a6534ac0466b5bc63e2b90
go0h <aks64@yandex.ru>
2019-06-27 23:44:48 +0300

    new logic

--- src/push_swap/solver.c
+++ src/push_swap/solver.c
@@ -8,77 +8,80 @@ void		ft_solver(t_stack **stack)
 	t_stack	*stack_a;
 	t_stack	*stack_b;
 	char	*line;
+	int	med;
 
 	if (!(line = ft_strnew(0)))
 		ft_exit(MALLOC_FAILURE);
 	stack_b = NULL;
 	stack_a = *stack;
-	ft_split_med(&stack_a, &stack_b, &line, 1);
-	line = ft_strjoin_f(line, "-\n");
-	while (ft_get_length_stack(stack_b) != 1)
-	{
-		ft_get_half_stack(&stack_b, &stack_a, &line, 0);
-	}
+	med = ft_get_mediana(stack_a);
+//	while (med)
+//	{
+		ft_split_med(&stack_a, &stack_b, &line, med);
+		while (ft_get_length_stack(stack_b) != 1)
+			ft_get_half_stack(&stack_b, &stack_a, &line, med);
+		ft_push(&stack_b, &stack_a);
+		ft_rotate(&stack_a);
+		med /= 2;
+//	}
 	ft_printf("%s", line);
 	*stack = stack_a;
 }
 
-void	ft_split_med(t_stack **s_a, t_stack **s_b, char **line, int f)
+void	ft_split_med(t_stack **s_a, t_stack **s_b, char **line, int med)
 {
-	int		med;
 	t_stack	*stack_a;
 	int		len;
-
+	
 	stack_a = *s_a;
 	len = ft_get_length_stack(stack_a);
 	len = (len % 2 == 1 && len != 1) ? len / 2 + 1 : len / 2;
-	med = ft_get_mediana(stack_a);
 	while (len)
 	{
 		ft_printstacks(stack_a, *s_b);
 		if (stack_a && stack_a->num <= med)
 		{
 			ft_push(&stack_a, s_b);
-			if (!(*line = ft_strjoin_f(*line, f ? "pb\n" : "pa\n")))
+			if (!(*line = ft_strjoin_f(*line, "pb\n")))
 				ft_exit(MALLOC_FAILURE);
 			--len;
 		}
 		else
 		{
 			ft_rotate(&stack_a);
-			if (!(*line = ft_strjoin_f(*line, f ? "ra\n" : "rb\n")))
+			if (!(*line = ft_strjoin_f(*line, "ra\n")))
 				ft_exit(MALLOC_FAILURE);
 		}
 	}
 	*s_a = stack_a;
 }
 
-void		ft_get_half_stack(t_stack **s_a, t_stack **s_b, char **line, int f)
+void		ft_get_half_stack(t_stack **s_a, t_stack **s_b, char **line, int med)
 {
 	int		len;
-	int		med;
+//	int		med;
 	t_stack *stack_a;
 
 	stack_a = *s_a;
-	len = ft_get_length_stack(stack_a) / 2;
+	len = ft_get_length_stack(stack_a) / 2 - 1;
 	med = ft_get_mediana(stack_a);
-	while (len)
+	while (ft_get_length_stack(stack_a) > len)
 	{
-		ft_printstacks(f ? stack_a : *s_b, f ? *s_b : stack_a);
+		ft_printstacks(*s_b, stack_a);
 		if (stack_a && stack_a->num <= med)
 		{
 			ft_rotate(&stack_a);
-			if (!(*line = ft_strjoin_f(*line, f ? "ra\n" : "rb\n")))
+			if (!(*line = ft_strjoin_f(*line, "ra\n")))
 				ft_exit(MALLOC_FAILURE);
 		}
 		else
 		{
 			ft_push(&stack_a, s_b);
-			if (!(*line = ft_strjoin_f(*line, f ? "pa\n" : "pb\n")))
+			if (!(*line = ft_strjoin_f(*line, "pa\n")))
 				ft_exit(MALLOC_FAILURE);
-			--len;
+//			--len;
 		}
 	}
-	ft_printstacks(f ? stack_a : *s_b, f ? *s_b : stack_a);
+	ft_printstacks(*s_b, stack_a);
 	*s_a = stack_a;
 }
